41eea89d68e563808dd66fe8eccdb91cfc4dc373
snapchtat <danny700218@gmail.com>
2026-03-31 02:38:43 +0000

    Initial commit

--- dashboard.py
+++ dashboard.py
@@ -3,6 +3,7 @@ from flask_cors import CORS
 import threading
 import logging
 import shared_data
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -24,7 +25,9 @@ def api_closed():
 def api_signals():
     return jsonify(shared_data.recent_signals[-20:])
 
-def run_dashboard(host='0.0.0.0', port=5000):
+
+def run_dashboard(host='0.0.0.0', port=None):
+    port = int(os.environ.get("PORT", port or 5000))
     app.run(host=host, port=port, debug=False, use_reloader=False)
 
 def start_dashboard():
